4c7dc94d41ef637846f93dabd7c91ed14520b718
Dmitry Brook <dmitrii_bryuhanov@mail.ru>
2021-05-01 23:57:22 +0300

    add: tests.sh

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
 mod errors;
 
-use rsevmasm::{Disassembly, Instruction};
 use errors::DisasmError;
+use num_bigint::BigUint;
+use rsevmasm::{Disassembly, Instruction};
 
-fn main() {
-    println!("Hello, world!");
+pub fn disassemble_evm(hex_data: &[u8]) -> Result<(), rsevmasm::DisassemblyError> {
+    for (addr, instruction) in Disassembly::from_bytes(hex_data)?.instructions.iter() {
+        match instruction {
+            Instruction::Push(arg) => println!(
+                "{:#x} PUSH {:#x}",
+                addr,
+                BigUint::from_bytes_be(arg.as_slice())
+            ),
+            Instruction::Dup(arg) => println!("{:#x} DUP {:#x}", addr, arg),
+            Instruction::Swap(arg) => println!("{:#x} SWAP {:#x}", addr, arg),
+            Instruction::Log(arg) => println!("{:#x} LOG {:#x}", addr, arg),
+            i => println!("{:#x} {}", addr, format!("{:?}", i).to_uppercase()),
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), DisasmError> {
+
+
+    Ok(())
 }
